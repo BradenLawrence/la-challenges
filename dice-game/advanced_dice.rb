27ac4ab6bbe60508224cf3eb099f4edecbd9ae57
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-21 17:33:14 -0400

    init advanced solution using dice.rb as a template

--- dice-game/advanced_dice.rb
+++ dice-game/advanced_dice.rb
@@ -1,1 +1,15 @@
-# YOUR ADVANCED CODE, HERE
+d6 = [1,2,3,4,5,6]
+roll = 2
+input = nil
+puts "Rolling a six sided die twice..."
+while input != "S" do
+  results = []
+  roll.times do
+    results.push( d6[rand(6)] )
+  end
+  total = results.reduce(:+)
+  puts "Roll results: #{results.join(', ')}\n" +
+       "Total: #{total}\n\n"                   +
+       "Press 'Enter' to roll again, or 'S' to stop."
+  input = gets.chomp.upcase
+end
