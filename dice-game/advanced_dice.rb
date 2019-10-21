f3003c3902865c90bef58e9271abe345bd4b031a
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-21 17:33:14 -0400

    add user input to determine # faces on die

--- dice-game/advanced_dice.rb
+++ dice-game/advanced_dice.rb
@@ -1,5 +1,22 @@
-d6 = [1,2,3,4,5,6]
-roll = 2
+puts "Dice-o-matic\n------------\n"
+# Determine faces
+user_sides = 0
+while user_sides < 1 do
+  puts "How many sides does your die have?"
+  print "> "
+  user_sides = gets.chomp.to_i
+  if user_sides < 1
+    puts "Sorry, you must enter a number greater than zero."
+  end
+end
+die = []
+user_sides.times do |face|
+  die.push(face+1)
+end
+
+user_rolls = nil
+roll = nil
+
 input = nil
 puts "Rolling a six sided die twice..."
 while input != "S" do
