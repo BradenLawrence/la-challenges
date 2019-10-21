44e6d6e66d65ad2ab6795c754f70b4befa492cec
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-21 15:07:39 -0400

    add repeated input prompt

--- infinite-questions/code.rb
+++ infinite-questions/code.rb
@@ -1,1 +1,10 @@
-# YOUR CODE HERE
+# PART 1
+puts "Golly, I sure love helping!"
+input = nil
+while input != "no" do
+  puts "Is there anything I can get you? (enter 'no' if finished)"
+  unless input == "no"
+    input = gets.chomp
+  end
+end
+puts "k, bye!"
