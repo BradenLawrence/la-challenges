ae549ee13be6c27acae72ce515f1db7a6588712b
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-21 17:33:14 -0400

    add new roll logic to use user inputs and repeating with new die

--- dice-game/advanced_dice.rb
+++ dice-game/advanced_dice.rb
@@ -1,41 +1,51 @@
 puts "Dice-o-matic\n------------\n"
-# Determine faces
-user_sides = 0
-while user_sides < 1 do
-  puts "How many sides does your die have?"
-  print "> "
-  user_sides = gets.chomp.to_i
-  if user_sides < 1
-    puts "Sorry, you must enter a number greater than zero."
-  end
-end
-die = []
-user_sides.times do |face|
-  die.push(face+1)
-end
 
-# Determine roll
-roll = 0
-while roll < 1 do
-  puts "How many times would you like to roll?"
-  print "> "
-  roll = gets.chomp.to_i
-  if roll < 1
-    puts "Sorry, you must enter a number greater than zero."
+continue_program = nil
+while continue_program != "S" do
+  # Determine faces
+  user_sides = 0
+  while user_sides < 1 do
+    puts "How many sides does your die have?"
+    print "> "
+    user_sides = gets.chomp.to_i
+    if user_sides < 1
+      puts "Sorry, you must enter a number greater than zero."
+    end
   end
-end
+  die = []
+  user_sides.times do |face|
+    die.push(face+1)
+  end
 
-# Start rolling
-input = nil
-puts "Rolling a #{user_sides} sided die #{roll} times..."
-while input != "S" do
-  results = []
-  roll.times do
-    results.push( d6[rand(6)] )
+  # Determine roll
+  roll = 0
+  while roll < 1 do
+    puts "How many times would you like to roll?"
+    print "> "
+    roll = gets.chomp.to_i
+    if roll < 1
+      puts "Sorry, you must enter a number greater than zero."
+    end
   end
-  total = results.reduce(:+)
-  puts "Roll results: #{results.join(', ')}\n" +
-       "Total: #{total}\n\n"                   +
-       "Press 'Enter' to roll again, or 'S' to stop."
-  input = gets.chomp.upcase
+
+  # Start rolling
+  continue_rolling = nil
+  puts "Rolling a #{user_sides} sided die #{roll} times..."
+  while continue_rolling != "S" do
+    results = []
+    roll.times do
+      results.push(  die[ rand(die.length) ]  )
+    end
+    total = results.reduce(:+)
+    puts %W[Roll results: #{results.join(', ')}\n
+      Total: #{total}\n\n
+            Press 'Enter' to roll again, or 'S' to stop.].join(' ')
+    print "> "
+    continue_rolling = gets.chomp.upcase
+  end
+
+  puts %W[Would you like to roll with a different die?\n
+          Press 'Enter' to create a new die or 'S' to quit.].join(' ')
+  print "> "
+  continue_program = gets.chomp.upcase
 end
