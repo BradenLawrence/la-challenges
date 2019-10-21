26638ab3808640675080182735b38bb7539a86da
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-21 17:25:47 -0400

    add dice roll logic for two results

--- dice-game/dice.rb
+++ dice-game/dice.rb
@@ -1,1 +1,11 @@
-# YOUR CODE, HERE
+d6 = [1,2,3,4,5,6]
+roll = 2
+input = nil
+while input != "S" do
+  results = []
+  roll.times do
+    results.push( d6[rand(6)] )
+  end
+  puts results
+  input = gets.chomp
+end
