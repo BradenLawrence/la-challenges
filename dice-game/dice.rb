f0890818c3aa80bf18be8e56b8667ce60b915bed
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-21 17:33:14 -0400

    add total score

--- dice-game/dice.rb
+++ dice-game/dice.rb
@@ -1,11 +1,15 @@
 d6 = [1,2,3,4,5,6]
 roll = 2
 input = nil
+puts "Rolling a six sided die twice..."
 while input != "S" do
   results = []
   roll.times do
     results.push( d6[rand(6)] )
   end
-  puts results
-  input = gets.chomp
+  total = results.reduce(:+)
+  puts "Roll results: #{results.join(', ')}\n" +
+       "Total: #{total}\n\n"                   +
+       "Press 'Enter' to roll again, or 'S' to stop."
+  input = gets.chomp.upcase
 end
