a175cc6154a2060d4b56f73386b6077059139064
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-22 15:29:50 -0400

    add intro and difficulty selection

--- guessing-game/code.rb
+++ guessing-game/code.rb
@@ -1,1 +1,23 @@
-# Your code goes here
+puts <<~INTRO
+  Terminal Telepathy
+  ------------------
+  I can sense a lingering telepathic energy from you...
+  Let's test your ability to read my mind!
+INTRO
+
+selection = nil
+difficulty = nil
+while selection.nil? do
+  puts "Select your difficulty ('easy' or 'hard')"
+  print "> "
+  selection = gets.chomp.downcase
+  case selection
+  when "easy"
+    difficulty = 10
+  when "hard"
+    difficulty = 20
+  else
+    puts "Sorry, that difficulty setting is unsupported. Please try again."
+    selection = nil
+  end
+end
