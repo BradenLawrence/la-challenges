10e4c04ee64a600e65fdd38b5a7109f0e7b36e90
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-22 16:57:19 -0400

    add solution

--- ninety-nine-bottles/code.rb
+++ ninety-nine-bottles/code.rb
@@ -1,1 +1,30 @@
-# YOUR CODE HERE
+bottles = 99
+excitement = nil
+
+while bottles>0 do
+  if bottles>1
+    if bottles % 10 == 0
+      excitement = "!"
+    else
+      excitement = "."
+    end
+    puts <<~LYRICS.squeeze(" ")
+    #{bottles} bottles of beer on the wall, #{bottles} bottles of \
+    beer#{excitement}
+    Take one down and pass it around, #{bottles-1} bottles of beer on the wall.
+
+    LYRICS
+  else
+    puts <<~LYRICS
+    #{bottles} bottle of beer on the wall, #{bottles} bottle of beer.
+    Take one down and pass it around. No more bottles of beer on the wall.
+
+    LYRICS
+  end
+  bottles -= 1
+end
+
+puts <<~ENDING
+  No more bottles of beer on the wall, no more bottles of beer.
+  Go to the store and buy some more, 99 bottles of beer on the wall.
+ENDING
