ba83144e390d567d8c3ae78880601c31c7745abb
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-22 14:26:08 -0400

    add some user-friendly input brackets

--- smart-vending-machine/code.rb
+++ smart-vending-machine/code.rb
@@ -1,8 +1,10 @@
 puts "Dispense-o-tron\n----------------\nHello! What can I get for you?"
+print "> "
 selection = gets.chomp
 puts "Sure thing! How many #{selection} would you like?"
 quantity = 0
 while quantity<1 do
+  print "> "
   quantity = gets.chomp.to_i
   if quantity<1
     puts "Sorry, you must enter a number greater than zero."
