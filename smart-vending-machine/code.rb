d654411d710bfea23b07b009b484d1064e0bc84d
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-22 14:23:44 -0400

    add solution for part 1

--- smart-vending-machine/code.rb
+++ smart-vending-machine/code.rb
@@ -1,1 +1,14 @@
-# YOUR CODE GOES HERE
+puts "Dispense-o-tron\n----------------\nHello! What can I get for you?"
+selection = gets.chomp
+puts "Sure thing! How many #{selection} would you like?"
+quantity = 0
+while quantity<1 do
+  quantity = gets.chomp.to_i
+  if quantity<1
+    puts "Sorry, you must enter a number greater than zero."
+  end
+end
+quantity.times do
+  puts selection
+end
+puts "Have a nice day!"
