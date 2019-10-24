f8aba985687cc728f96399703b08333e5f20ab31
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-24 11:40:21 -0400

    start petflix-night challenge

---
+++ petflix-night/code.rb
@@ -0,0 +1,53 @@
+dramas = [
+  "Stanger Llamas",
+  "Game of Bones",
+  "Llama Actually",
+  "Grey's Catanomy",
+  "Breaking Baaaaahhhd"
+]
+
+animations = [
+  "Bojack Man",
+  "Family Dog",
+  "Futurellama",
+  "Barkcher",
+  "American Dog",
+  "Pokemon",
+  "Bob's Barkers"
+]
+
+comedies = [
+  "Barks and Recreation",
+  "30 Cats",
+  "Bones",
+  "Girl meets Cat",
+  "Gilmore Cats",
+  "New Dog",
+  "Catlandia",
+  "Full Mouse"
+]
+
+all_shows = [
+  "Stanger Llamas",
+  "Game of Bones",
+  "Llama Actually",
+  "Grey's Catanomy",
+  "Breaking Baaaaahhhd",
+  "Bojack Man",
+  "Family Dog",
+  "Futurellama",
+  "Barkcher",
+  "American Dog",
+  "Pokemon",
+  "Bob's Barkers",
+  "Barks and Recreation",
+  "30 Cats",
+  "Bones",
+  "Girl meets Cat",
+  "Gilmore Cats",
+  "New Dog",
+  "Catlandia",
+  "Full Mouse"
+]
+
+#YOUR CODE GOES HERE
