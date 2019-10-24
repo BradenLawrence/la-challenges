14bfb33c87479b9bde20f029fd47b6a920edebe0
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-24 17:06:45 -0400

    add introduction and get user's name

---
+++ time-capsule/code.rb
@@ -0,0 +1,20 @@
+time_capsule = Array.new
+
+# Greet the user and take their name. Store this name.
+puts %W(
+  Chrono-box 9000
+  ---------------\n
+  Welcome! Are you ready to save some time?!
+  Let's get started!\n
+  Could I get your name please?
+).join(" ")
+print "> "
+name = gets.chomp
+
+# Ask the user what they want to add to the time capsule. Give the user an option to type the command "FINISHED" which instructs the program to stop asking the user to add more items.
+
+
+# If they entered an item, store that item in the time capsule.
+
+
+# Once finished, print out the contents of the time capsule.
