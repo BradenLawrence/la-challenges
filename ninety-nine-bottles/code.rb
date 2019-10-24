d91255e775ad477d662810538c348ff10b9f68f7
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-24 10:17:47 -0400

    add plurality checker to eliminate repetition of the lyrics

--- ninety-nine-bottles/code.rb
+++ ninety-nine-bottles/code.rb
@@ -1,21 +1,18 @@
 bottles = 99
+plural = "s"
 
 while bottles>0 do
-  if bottles>1
     excitement = bottles % 10 == 0 ? "!" : "."
-    puts <<~LYRICS.squeeze(" ")
-      #{bottles} bottles of beer on the wall, #{bottles} bottles of \
-      beer#{excitement}
-      Take one down and pass it around, #{bottles-1} bottles of beer on the \
-      wall.\n
-    LYRICS
-  else
-    puts <<~LYRICS
-      #{bottles} bottle of beer on the wall, #{bottles} bottle of beer.
-      Take one down and pass it around. No more bottles of beer on the wall.\n
-    LYRICS
-  end
-  bottles -= 1
+    puts <<~VERSE1.squeeze(" ")
+      #{bottles} bottle#{plural} of beer on the wall, #{bottles} \
+      bottle#{plural} of beer#{excitement}
+    VERSE1
+    bottles -= 1
+    plural = bottles == 1 ? "" : "s"
+    puts <<~VERSE2.squeeze(" ")
+      Take one down and pass it around, #{bottles} bottle#{plural} of beer on \
+      the wall.\n
+    VERSE2
 end
 
 puts <<~ENDING
