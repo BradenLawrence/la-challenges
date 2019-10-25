68f2c9c0edef5fd32489b250b801f19db7763d17
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-25 15:29:55 -0400

    add journey lyrics

--- next-flight/code.rb
+++ next-flight/code.rb
@@ -1,4 +1,24 @@
 flights = [2,5,7.5,8.5,9,10,11.5,13.5,14.5,17,18,19,24]
+journey = <<~LYRICS
+  ***DON'T STOP...BELIEVIN'!***
+
+  Just a small town girl
+  Living in a lonely world
+  She took the midnight train going anywhere
+  Just a city boy
+  Born and raised in South Detroit
+  He took the midnight train going anywhere
+
+  A singer in a smoky room
+  A smell of wine and cheap perfume
+  For a smile they can share the night
+  It goes on and on and on and on
+
+  Strangers waiting, up and down the boulevard
+  Their shadows searching in the night
+  Streetlights people, living just to find emotion
+  Hiding, somewhere in the night.
+LYRICS
 
 puts %Q(
   Howdy! Welcome to Crazy Neds Discount Space Travel!
