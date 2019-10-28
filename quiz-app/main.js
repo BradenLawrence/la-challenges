1f3b645ef36f0ba2787c93c60a9a5f769f53ab36
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 15:32:33 -0400

    add team1 average scores

--- quiz-app/main.js
+++ quiz-app/main.js
@@ -1,15 +1,3 @@
-// Team 1 - JustJavaScript
-// Rami: 88
-// Natoya: 92
-// Max: 68
-// Lynn: 100
-//
-// Team 2 - We Are Ruby
-// Sasha: 82
-// Mohammed: 98
-// Jennifer: 94
-// Denise: 92
-
 let team1 = {
   name: "JustJavaScript",
   scores: {
@@ -32,4 +20,10 @@ let team2 = {
 
 // * calculate the average score for each team and for both teams together
 // * round the averages to the nearest integer for display
+let team1_scores = Object.values(team1.scores)
+let team1_avg =  team1_scores.reduce((total, current) => {
+  return total += current
+}, 0) / team1_scores.length)
+
+
 // * calculate the number of explorers passing the quiz (grade >= 70%)
