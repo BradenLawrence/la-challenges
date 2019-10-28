c7161f3cc56cc1d1c8363223ff91e07ab146456e
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 15:55:42 -0400

    add team objs to arr, display formatted results

--- quiz-app/main.js
+++ quiz-app/main.js
@@ -1,22 +1,23 @@
-let team1 = {
-  name: "JustJavaScript",
-  scores: {
-    Rami: 88,
-    Natoya: 92,
-    Max: 68,
-    Lynn: 100
+let teams = [
+  team1 = {
+    name: "JustJavaScript",
+    scores: {
+      Rami: 88,
+      Natoya: 92,
+      Max: 68,
+      Lynn: 100
+    }
+  },
+  team2 = {
+    name: "We Are Ruby",
+    scores: {
+      Sasha: 82,
+      Mohammed: 98,
+      Jennifer: 94,
+      Denise: 92
+    }
   }
-}
-
-let team2 = {
-  name: "We Are Ruby",
-  scores: {
-    Sasha: 82,
-    Mohammed: 98,
-    Jennifer: 94,
-    Denise: 92
-  }
-}
+]
 
 // * calculate the average score for each team and for both teams together
 // * round the averages to the nearest integer for display
@@ -26,9 +27,6 @@ const average = (obj) => {
     return total += current
   }, 0) / scores.length)
 }
-
-console.log(average(team1.scores))
-console.log(average(team2.scores))
 
 // * calculate the number of explorers passing the quiz (grade >= 70%)
 const passing = (obj) => {
@@ -42,5 +40,8 @@ const passing = (obj) => {
   }, 0))
 }
 
-console.log(passing(team1.scores))
-console.log(passing(team2.scores))
+teams.forEach(team => {
+  console.log(`${team.name} stats\n--------------------
+Average Score: ${average(team.scores)}
+Passing Scores: ${passing(team.scores)}\n\n`)
+})
