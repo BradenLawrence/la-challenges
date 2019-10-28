5953dfd71cc41b16da88f3d57bb9d883202cc662
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 15:08:04 -0400

    add instructions and team objects

--- quiz-app/main.js
+++ quiz-app/main.js
@@ -1,1 +1,35 @@
-// your code, here
+// Team 1 - JustJavaScript
+// Rami: 88
+// Natoya: 92
+// Max: 68
+// Lynn: 100
+//
+// Team 2 - We Are Ruby
+// Sasha: 82
+// Mohammed: 98
+// Jennifer: 94
+// Denise: 92
+
+let team1 = {
+  name: "JustJavaScript",
+  scores: {
+    Rami: 88,
+    Natoya: 92,
+    Max: 68,
+    Lynn: 100
+  }
+}
+
+let team2 = {
+  name: "We Are Ruby",
+  scores: {
+    Sasha: 82,
+    Mohammed: 98,
+    Jennifer: 94,
+    Denise: 92
+  }
+}
+
+// * calculate the average score for each team and for both teams together
+// * round the averages to the nearest integer for display
+// * calculate the number of explorers passing the quiz (grade >= 70%)
