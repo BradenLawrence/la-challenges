0bd673acd7412f07d85c136e938f41067f4529f1
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 14:28:59 -0400

    add random number, user prompt and display results

--- guess-the-number-javascript/main.js
+++ guess-the-number-javascript/main.js
@@ -1,1 +1,13 @@
-// YOUR CODE HERE
+secret = Math.floor(Math.random() * 10 + 1)
+
+console.log(`Thank you for participating in this psychic ability assessment!`)
+user = prompt(`Please enter your name:`)
+guess = prompt(`Thank you! Now, focus your psycic powers and guess the\
+  secret number between 1 and 10:`)
+
+if(secret === guess) {
+  console.log(`Congrats ${user}! You might be psycic!`)
+} else {
+  console.log(`Sorry ${user}, you guessed incorrectly and have been \
+disqualified.`)
+}
