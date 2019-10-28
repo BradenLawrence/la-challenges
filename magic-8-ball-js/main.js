be01c149d237c520eb3ed57ba8d45145352a6d31
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 18:09:36 -0400

    remove instruction comments

--- magic-8-ball-js/main.js
+++ magic-8-ball-js/main.js
@@ -20,34 +20,27 @@ let responses = [
   "Outlook not so good",
   "Very doubtful"
 ]
-// your code, here
-// Confirm the correct number of responses have been stored by printing the number to the console (`20`)
 console.log(`Greetings! I am Octavius Ball, reknowned fortune teller!
 I knew you would be stopping by, so I have prepared a dazzling array of \
 ${responses.length} possible futures for you.`)
 
-// Ask the user what their question is, and store it as a variable called `question`
 let question = prompt(`What do you wish to know?`)
 
-// Output a random response to the user's question
 console.log(`Casting your question into the threads of fate...
 \t~ '${question}' ~`)
 console.log(responses[Math.floor(Math.random()*responses.length)])
 
-// Output the number of responses that include the word "yes"
 let positive = responses.filter(response => {
   return response.toLowerCase().includes(`yes`)
 })
 console.log(`Responses including 'yes': ${positive.length}`)
 
-// Output the number of responses that include the words "no" or "not"
 let negative = responses.filter(response => {
   return response.toLowerCase().includes(`no`) ||
          response.toLowerCase().includes(`not`)
 })
 console.log(`Responses including 'no' or 'not': ${negative.length}`)
 
-// Output the alphabetized list of responses
 responses.sort()
 let alpha_list = responses.reduce((list, response) => {
   return list += `* ${response}\n`
@@ -55,13 +48,11 @@ let alpha_list = responses.reduce((list, response) => {
 console.log(`Behold the other fates that might have befallen you:
 ${alpha_list}`)
 
-// Output the first and last responses from the alphabetized list
 console.log(`Take special note of these two, for they are the first and last of\
  your possible fates`)
 console.log(`* ${responses[0]}`)
 console.log(`* ${responses[responses.length-1]}`)
 
-// Delete two of the negative responses
 console.log(`Do not despair! You are not doomed to this fate!
 Watch as I bend the fabric of time and space to my will, and remove two of \
 your negative fates:`)
@@ -80,7 +71,6 @@ randomNegatives.forEach(negative => {
 })
 console.log(responses)
 
-// Reassign 'Ask again later' to the beginning of the array
 console.log(`Watch as I shuffle your fates as if they were a deck of cards!`)
 console.log(`Position of 'Ask again later': \
 ${responses.indexOf('Ask again later')}\n\t~ MAGIC ~`)
@@ -92,13 +82,11 @@ responses.unshift(
 console.log(`Position of 'Ask again later': \
 ${responses.indexOf('Ask again later')}`)
 console.log(responses)
-// Add 1 response of your choosing to the current list
 console.log(`Watch as I include a new possible future for you! \
 'You will win the lottery!'`)
 responses.push('You will win the lottery!')
 console.log(responses)
 
-// Concat an **array** of 3 additional responses to the current list
 let sorcery = [
   "You will find a puppy!",
   "You will find true love!",
@@ -108,6 +96,5 @@ console.log(`I'm just getting started! Here are three more possible futures \
 that I will grant you!\n${sorcery}`)
 console.log(responses.concat(sorcery))
 
-// Confirm the correct number of responses have been removed and added by printing the number of responses to the console (`22`)
-console.log(`Behold the new scope of possible futures you hold: 
+console.log(`Behold the new scope of possible futures you hold:
 ${responses.length}`)
