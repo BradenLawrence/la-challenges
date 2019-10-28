4cf2d8837c6faba49a4275129f97eae959483aab
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 14:43:27 -0400

    add variable declarations and cast input as a number

--- guess-the-number-javascript/main.js
+++ guess-the-number-javascript/main.js
@@ -1,9 +1,9 @@
-secret = Math.floor(Math.random() * 10 + 1)
+let secret = Math.floor(Math.random() * 10 + 1)
 
 console.log(`Thank you for participating in this psychic ability assessment!`)
-user = prompt(`Please enter your name:`)
-guess = prompt(`Thank you! Now, focus your psycic powers and guess the\
-  secret number between 1 and 10:`)
+let user = prompt(`Please enter your name:`)
+let guess = Number(prompt(`Thank you! Now, focus your psycic powers and guess the\
+  secret number between 1 and 10:`))
 
 if(secret === guess) {
   console.log(`Congrats ${user}! You might be psycic!`)
