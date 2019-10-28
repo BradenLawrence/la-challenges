ff228e9df57334b430008751594347c74a8ec56c
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-28 11:30:19 -0400

    add user input for flightLeader and display it.

---
+++ prep-js-input-and-output/main.js
@@ -0,0 +1,5 @@
+let flightLeader
+flightLeader = prompt('Who is this excursion\'s flight leader?')
+confirm('Are you sure this is the correct flight leader?')
+alert(`${flightLeader} is leading this excursion.`)
+console.log(`Today, your flight leader is ${flightLeader}`)
