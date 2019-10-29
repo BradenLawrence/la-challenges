3cac2a696b10ae9e466746e635a267deaae3aed0
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-29 10:35:17 -0400

    refactor logic into functions

--- ninety-nine-bottles-js/main.js
+++ ninety-nine-bottles-js/main.js
@@ -1,30 +1,55 @@
-let initial_quantity = 99,
-    bottles = initial_quantity,
-    plural = "s",
-    excitement = ".",
-    singing = true
+let initial_qty = 99
 
-while(singing) {
-  let verse = ``
-  let line1 = `${bottles} bottle${plural} of beer on the wall,\
- ${Number.isInteger(bottles) ? bottles : bottles.toLowerCase()} bottle${plural}\
- of beer${excitement}\n`
+const lyric = (num) => {
+  if(num > 0) {
+    return num
+  } else if(num === 0) {
+    return "No more"
+  } else {
+    return initial_qty
+  }
+}
+
+const plural = (num) => {
+  if(num === 1) {
+    return ""
+  } else {
+    return "s"
+  }
+}
+
+const downcase = (lyric) => {
+  if(Number.isInteger(lyric)) {
+    return lyric
+  } else {
+    return lyric.toLowerCase()
+  }
+}
+
+const excitement = (num) => {
+  if(num % 10 === 0) {
+    return "!"
+  } else {
+    return "."
+  }
+}
+
+for(let i = initial_qty; i >= 0; i--) {
+  let verse = ""
+  let line1 = `${lyric(i)} bottle${plural(i)} of beer on the wall, \
+${downcase(lyric(i))} bottle${plural(i)} of beer${excitement(lyric(i))}\n`
   verse += line1
-  let line2 = null
-  if(Number.isInteger(bottles)) {
+
+  let line2 = ""
+  if(i === lyric(i)) {
     line2 = "Take one down and pass it around, "
   } else {
     line2 = "Go to the store and buy some more, "
-    bottles = initial_quantity + 1
-    singing = false
   }
   verse += line2
-  bottles = bottles > 1 ? bottles - 1 : "No more"
-  plural = bottles == 1 ? "" : "s"
-  excitement = bottles % 10 == 0 ? "!" : "."
 
-  let line3 = `${Number.isInteger(bottles) ? bottles : bottles.toLowerCase()}\
- bottle${plural} of beer on the wall.\n\n`
+  let line3 = `${downcase(lyric(i-1))} bottle${plural(i-1)} of beer on \
+the wall.\n\n`
   verse += line3
   console.log(verse)
 }
