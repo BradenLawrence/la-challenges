54dca567113f55854ad6712f7a18e9f8d0fd987f
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-29 15:41:23 -0400

    add perimeter function with three examples

--- perimeter-function-js/main.js
+++ perimeter-function-js/main.js
@@ -1,1 +1,12 @@
-// your code, here
+const perimeter = (width, height) => {
+  return (width + height) * 2
+}
+
+console.log("Rectangle with width of 8 and height of 4:")
+console.log(perimeter(8, 4))
+
+console.log("Rectangle with width of 320 and height of 7:")
+console.log(perimeter(320, 7))
+
+console.log("Rectangle with width of 45.2 and height of 76.5:")
+console.log(perimeter(45.2, 76.5))
