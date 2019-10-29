6d7213d8ee91a00b331cf97580098fd7b860a311
Braden Lawrence <lawrencebradenm@gmail.com>
2019-10-29 13:31:44 -0400

    refactor how team data is structured and update helpers accordingly

--- quiz-app/main.js
+++ quiz-app/main.js
@@ -1,44 +1,53 @@
 let teams = [
   team1 = {
     name: "JustJavaScript",
-    scores: {
-      Rami: 88,
-      Natoya: 92,
-      Max: 68,
-      Lynn: 100
-    }
+    scores: [
+      { player: "Rami",     score: 88  },
+      { player: "Natoya",   score: 92  },
+      { player: "Max",      score: 68  },
+      { player: "Lynn",     score: 100 }
+    ]
   },
   team2 = {
     name: "We Are Ruby",
-    scores: {
-      Sasha: 82,
-      Mohammed: 98,
-      Jennifer: 94,
-      Denise: 92
-    }
+    scores: [
+      { player: "Sasha",    score: 82  },
+      { player: "Mohammed", score: 98  },
+      { player: "Jennifer", score: 94  },
+      { player: "Denise",   score: 92  }
+    ]
   }
 ]
 
 const average = (obj) => {
-  let scores = Object.values(obj)
-  return Math.floor(scores.reduce((total, current) => {
-    return total += current
-  }, 0) / scores.length)
+  let total = 0
+  obj.scores.forEach(item => {
+    total += item.score
+  })
+  obj.average = total / obj.scores.length
+  return obj.average
 }
 
 const passing = (obj) => {
-  let scores = Object.values(obj)
-  return Math.floor(scores.reduce((total, current) => {
-    if(current >= 70) {
-      return total += 1
+  obj.totalPassing = 0
+  obj.scores.forEach(item => {
+    if(item.score >= 70) {
+      item.passing = true
+      obj.totalPassing++
     } else {
-      return total
+      item.passing = false
     }
-  }, 0))
+  })
+  return obj.passing
 }
 
 teams.forEach(team => {
+  average(team)
+  passing(team)
   console.log(`${team.name} stats\n--------------------
-Average Score: ${average(team.scores)}
-Passing Scores: ${passing(team.scores)}\n\n`)
+Average Score: ${team.average}`)
+  team.scores.forEach(item => {
+    console.log(`${item.player}: ${item.passing ? "pass" : "fail"}`)
+  })
+  console.log(`Total Passing Scores: ${team.totalPassing}\n\n`)
 })
