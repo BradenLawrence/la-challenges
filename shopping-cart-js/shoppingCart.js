fff4ad537ac7dd266f5f3458373c2603ea83efc1
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-04 17:41:38 -0500

    add shopping item objects

--- shopping-cart-js/shoppingCart.js
+++ shopping-cart-js/shoppingCart.js
@@ -1,1 +1,16 @@
-// your code, here
+let coffee = {
+  itemName: "Gevalia Costa Rican Light Roast Coffee",
+  quantity: 10
+}
+let apples = {
+  itemName: "Honeycrisp Apple",
+  quantity: 6
+}
+let hummus = {
+  itemName: "Sabra Roasted Garlic Hummus",
+  quantity: 1
+}
+let crackers = {
+  itemName: "Wasa Multi Grain Crispbread",
+  quantity: 1
+}
