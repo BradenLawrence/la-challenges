d499a4ccd365c597d5dc4941a130eb0f0d4235c8
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-05 11:38:56 -0500

    start js-array-drills challenge

---
+++ js-array-drills/js-array-drills.md
@@ -0,0 +1,33 @@
+Practice working with arrays in JavaScript!
+
+## Getting Started
+```no-highlight
+et get js-array-drills
+cd js-array-drills
+atom .
+open index.html
+```
+
+>Your work should go in the `main.js` file
+
+### Instructions
+
+You've been provided with two arrays in the `main.js` file. The first array is a list of teachers. The second is a list of rooms, and each room contains student names.
+
+**The index of the teacher correlates to the room of students they teach.** That is, if teacher Arrington is at `teachers[0]` then the room she teaches is `rooms[0]`.
+
+The functions have already been set up for you. Your job is to complete them to return the information requested. The instructions are also commented for you in the `main.js` file.
+
+* List all the teachers with an even index number (including 0)
+
+* List all of the teachers with the letter i in their name
+
+* Return the number of teachers
+
+* Return the number of rooms of students
+
+* Return the number of students in all the rooms
+
+* Return the students who have an i in their names
+
+* Return the teacher who has the given student in their room
---
+++ js-array-drills/main.js
@@ -0,0 +1,65 @@
+let teachers = ["Arrington", "Kincart", "Alberts", "Pickett"]
+
+let rooms = [
+  ["Andy", "Rodolfo", "Lynn", "Talia"],
+  ["Al", "Ross", "Jorge", "Dante"],
+  ["Nick", "Kim", "Jasmine", "Dorothy"],
+  ["Adam", "Grayson", "Aliyah", "Alexa"]
+]
+
+//List all the teachers with an even index number (including 0)
+
+let evenIndex = () => {
+
+}
+
+evenIndex()
+
+//List all of the teachers with the letter i in their name
+
+let iInName = () => {
+
+}
+
+iInName()
+
+//Return the number of teachers
+
+let teacherCount = () => {
+
+}
+
+console.log(teacherCount())
+
+//Return the number of rooms of students
+
+let roomCount = () => {
+
+}
+
+console.log(roomCount())
+
+//Return the number of students in all the rooms
+
+let studentCount = () => {
+
+}
+
+console.log(studentCount())
+
+//Return the students who have an i in their names
+
+let whichStudents = () => {
+
+}
+
+console.log(`The students who have an 'i' in their name are  ${whichStudents()}.`)
+
+//Return the teacher who has the given student in their room
+
+let whichTeacher = (student) => {
+
+}
+
+console.log(`The teacher who has Jorge is ${whichTeacher("Jorge")}.`)
+console.log(`The teacher who has Alexa is ${whichTeacher("Alexa")}.`)
