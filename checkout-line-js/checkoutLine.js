4cd673637231aba019d47c8b99e512fb04bd5a6e
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-05 10:23:28 -0500

    add helper function to return quantity of an item.

--- checkout-line-js/checkoutLine.js
+++ checkout-line-js/checkoutLine.js
@@ -27,10 +27,17 @@ let shoppingCart = [eggs, milk, bread, coffee]
 // * How much is one pound (lb.) of coffee?
 const getPrice = (cart, name) => {
   return cart.find(item => {
-    return item.itemName == name
+    return item.itemName === name
   }).price
 }
 console.log(`1 ${coffee.itemName}: \$${getPrice(shoppingCart, coffee.itemName)}`)
 console.log(`1 ${milk.itemName}: \$${getPrice(shoppingCart, milk.itemName)}`)
 
 // * How many gallons of milk are in the shopping cart?
+const getQty = (cart, name) => {
+  return cart.find(item => {
+    return item.itemName === name
+  }).quantity
+}
+console.log(`${milk.itemName} qty: ${getQty(shoppingCart, milk.itemName)}`)
+console.log(`${coffee.itemName} qty: ${getQty(shoppingCart, coffee.itemName)}`)
