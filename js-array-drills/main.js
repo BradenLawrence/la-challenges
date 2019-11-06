818a53752f651bdf43e588e02abf885f98528c22
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-06 10:42:52 -0500

    remove instruction comments

--- js-array-drills/main.js
+++ js-array-drills/main.js
@@ -7,43 +7,33 @@ let rooms = [
   ["Adam", "Grayson", "Aliyah", "Alexa"]
 ]
 
-//List all the teachers with an even index number (including 0)
-
 let evenIndex = (arr) => {
   return arr.filter((item, index) => {
     return index % 2 === 0
   })
 }
-
 console.log(`Teachers with an even index: ${evenIndex(teachers)}`)
 
-//List all of the teachers with the letter i in their name
 let teacherFilter = "i"
 let iInName = (arr, str) => {
   return arr.filter(item => {
     return item.toLowerCase().includes(str)
   })
 }
-
 console.log(`Teachers with 'i' in name: ${iInName(teachers, teacherFilter)}`)
 
-//Return the number of teachers
 
 let teacherCount = () => {
   return teachers.length
 }
-
 console.log(`Number of teachers: ${teacherCount()}`)
 
-//Return the number of rooms of students
 
 let roomCount = () => {
   return rooms.length
 }
-
 console.log(`Number of rooms: ${roomCount()}`)
 
-//Return the number of students in all the rooms
 
 let studentCount = (arr) => {
   let total = 0
@@ -52,10 +42,8 @@ let studentCount = (arr) => {
   })
   return total
 }
-
 console.log(`Total students: ${studentCount(rooms)}`)
 
-//Return the students who have an i in their names
 let studentFilter = "i"
 let whichStudents = (groups, str) => {
   let filtered = []
@@ -68,11 +56,8 @@ let whichStudents = (groups, str) => {
   })
   return filtered
 }
-
 console.log(`The students who have an 'i' in their name are \
 ${whichStudents(rooms, studentFilter)}.`)
-
-//Return the teacher who has the given student in their room
 
 let whichTeacher = (student) => {
   let roomIndex = -1
@@ -83,6 +68,5 @@ let whichTeacher = (student) => {
   })
   return teachers[roomIndex]
 }
-
 console.log(`The teacher who has Jorge is ${whichTeacher("Jorge")}.`)
 console.log(`The teacher who has Alexa is ${whichTeacher("Alexa")}.`)
