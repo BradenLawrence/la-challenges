4c0cbcc1a647475cad0ebaf4d227b31fefbc0409
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-11 15:24:26 -0500

    add hash of lasers and their ranges

--- laser-advisor/code.rb
+++ laser-advisor/code.rb
@@ -1,1 +1,14 @@
-# your code, here
+laser_arsenal = {
+  blaster: 200,
+  helium_neon: 180,
+  nuclear_pumped: 170,
+  krypton: 170,
+  co2: 160,
+  coil: 150,
+  strontium_vapor: 140,
+  ruby: 130,
+  xenon_ion: 120,
+  free_electron: 110,
+  gas_dynamic: 95,
+  nitrogen: 0
+}
