fca9741445dde3fefc6d11fa379ccaacab1ac6f2
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-11 15:44:12 -0500

    sort hash by range

--- laser-advisor/code.rb
+++ laser-advisor/code.rb
@@ -1,14 +1,17 @@
 laser_arsenal = {
-  blaster: 200,
-  helium_neon: 180,
-  nuclear_pumped: 170,
-  krypton: 170,
-  co2: 160,
-  coil: 150,
-  strontium_vapor: 140,
-  ruby: 130,
-  xenon_ion: 120,
-  free_electron: 110,
-  gas_dynamic: 95,
-  nitrogen: 0
+  blaster:          200,
+  helium_neon:      180,
+  nuclear_pumped:   170,
+  krypton:          170,
+  co2:              160,
+  coil:             150,
+  strontium_vapor:  140,
+  ruby:             130,
+  xenon_ion:        120,
+  free_electron:    110,
+  gas_dynamic:       95,
+  nitrogen:           0
 }
+
+sorted_arsenal = laser_arsenal.sort_by {|key, value| value}.to_h
+puts sorted_arsenal
