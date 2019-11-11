c1b67a64dd7166326d5bd4ab0d5a134aa2209803
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-11 15:02:06 -0500

    add crew contacts hash

---
+++ invite-the-crew/code.rb
@@ -0,0 +1,8 @@
+crew_contacts = {
+  jose: 'workerNaut@example.com',
+  samantha: 'spaceFlightRacer@example.com',
+  talia: 'pro_space89@example.com',
+  mike: 'alwaysAtTheHelm@example.com',
+  olivia: 'maintainingMaintenance@example.com',
+  joan: 'bestNavigatorEver@example.com'
+}
