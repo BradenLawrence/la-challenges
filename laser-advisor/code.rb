e65c3af91666d93f81c06784ee8c3eff1ae7da41
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-11 17:37:37 -0500

    rename keys and remove commented hash

--- laser-advisor/code.rb
+++ laser-advisor/code.rb
@@ -1,31 +1,16 @@
-# laser_arsenal = {
-#   blaster:          200,
-#   helium_neon:      180,
-#   nuclear_pumped:   170,
-#   krypton:          170,
-#   co2:              160,
-#   coil:             150,
-#   strontium_vapor:  140,
-#   ruby:             130,
-#   xenon_ion:        120,
-#   free_electron:    110,
-#   gas_dynamic:       95,
-#   nitrogen:           0
-# }
-
 laser_arsenal = [
-  {laser: "blaster",         range: 200},
-  {laser: "helium neon",     range: 180},
-  {laser: "nuclear pumped",  range: 170},
-  {laser: "krypton",         range: 170},
-  {laser: "co2",             range: 160},
-  {laser: "coil",            range: 150},
-  {laser: "strontium vapor", range: 140},
-  {laser: "ruby",            range: 130},
-  {laser: "xenon ion",       range: 120},
-  {laser: "free electron",   range: 110},
-  {laser: "gas dynamic",     range:  95},
-  {laser: "nitrogen",        range:   0}
+  {name: "blaster",         range: 200},
+  {name: "helium neon",     range: 180},
+  {name: "nuclear pumped",  range: 170},
+  {name: "krypton",         range: 170},
+  {name: "co2",             range: 160},
+  {name: "coil",            range: 150},
+  {name: "strontium vapor", range: 140},
+  {name: "ruby",            range: 130},
+  {name: "xenon ion",       range: 120},
+  {name: "free electron",   range: 110},
+  {name: "gas dynamic",     range:  95},
+  {name: "nitrogen",        range:   0}
 ]
 
 laser_arsenal = laser_arsenal.sort_by {|obj| obj[:range]}
