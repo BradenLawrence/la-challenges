7491447027ffc3e0ad718c1ac7ee74f491b679c3
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-12 17:24:19 -0500

    add scorecard hash

---
+++ golf-score/code.rb
@@ -0,0 +1,20 @@
+scorecard = [
+  {par: 5, score: 7}
+  {par: 4, score: 5}
+  {par: 3, score: 3}
+  {par: 4, score: 4}
+  {par: 4, score: 4}
+  {par: 3, score: 2}
+  {par: 4, score: 5}
+  {par: 5, score: 5}
+  {par: 4, score: 5}
+  {par: 5, score: 7}
+  {par: 4, score: 4}
+  {par: 4, score: 4}
+  {par: 3, score: 3}
+  {par: 4, score: 5}
+  {par: 4, score: 5}
+  {par: 4, score: 4}
+  {par: 3, score: 3}
+  {par: 5, score: 6}
+]
