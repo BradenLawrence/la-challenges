5bcd6fb1466fbcb9f8007c14c9f2f21a5f2f05b9
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-12 14:27:58 -0500

    add answers to last set of questions

--- iteration-drills/code.rb
+++ iteration-drills/code.rb
@@ -72,12 +72,22 @@
 array = [28214, 63061, 49928, 98565, 31769, 42316, 23674, 3540, 34953, 70282, 22077, 94710, 50353, 17107, 73683, 33287, 44575, 83602, 33350, 46583]
 
 # * What is the sum of all the numbers in `array`?
-# * How would you print out each value of the array?
-# * What is the sum of all of the even numbers?
-# * What is the sum of all of the odd numbers?
-# * What is the sum of all the numbers divisble by 5?
-# * What is the sum of the squares of all the numbers in the array?
+# puts array.reduce(:+)
 
+# * How would you print out each value of the array?
+# array.each {|value| puts value}
+
+# * What is the sum of all of the even numbers?
+# puts array.select {|number| number % 2 == 0}.reduce(:+)
+
+# * What is the sum of all of the odd numbers?
+# puts array.select {|number| number % 2 != 0}.reduce(:+)
+
+# * What is the sum of all the numbers divisble by 5?
+# puts array.select {|number| number % 5 == 0}.reduce(:+)
+
+# * What is the sum of the squares of all the numbers in the array?
+# puts array.map {|number| number * number}.reduce(:+)
 
 
 
@@ -86,8 +96,20 @@ array = [28214, 63061, 49928, 98565, 31769, 42316, 23674, 3540, 34953, 70282, 22
 array = ["joanie", "annamarie", "muriel", "drew", "reva", "belle", "amari", "aida", "kaylie", "monserrate", "jovan", "elian", "stuart", "maximo", "dennis", "zakary", "louvenia", "lew", "crawford", "caitlyn"]
 
 # * How would you print out each name backwards in `array`?
+# array.each {|name| puts name.reverse}
+
 # * What are the total number of characters in the names in `array`?
+# puts array.reduce(0) {|total, name| total + name.length}
+
 # * How many names in `array` are less than 5 characters long?
+# puts array.select {|name| name.length < 5}.length
+
 # * How many names in `array` end in a vowel?
+# vowels = ["a","e","i","o","u"]
+# puts array.select {|name| vowels.include? name[-1]}.length
+
 # * How many names in `array` are more than 5 characters long?
+# puts array.select {|name| name.length > 5}.length
+
 # * How many names in `array` are exactly 5 characters in length?
+# puts array.select {|name| name.length == 5}.length
