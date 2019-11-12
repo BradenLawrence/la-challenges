45ed2df55351d25ddd79c598ec328f4b1a425f94
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-12 17:28:56 -0500

    add display for total strokes

--- golf-score/code.rb
+++ golf-score/code.rb
@@ -18,3 +18,10 @@ scorecard = [
   {par: 3, strokes: 3},
   {par: 5, strokes: 6}
 ]
+
+# Total Strokes: 81
+# Total Par: 72
+# You were 9 over par.
+
+total_strokes = scorecard.reduce(0) {|total, hole| total + hole[:strokes]}
+puts "Total Strokes: #{total_strokes}"
