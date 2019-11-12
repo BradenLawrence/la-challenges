f830769fee789f1e1645e328adc2337507a83d7b
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-11 21:32:45 -0500

    start iteration-drills challenge

---
+++ iteration-drills/code.rb
@@ -0,0 +1,80 @@
+#  Write Ruby<->English answers here as comments in your code
+
+# 1)
+# * numbers is an array
+# numbers.each do |number|
+#   puts 3 * number
+# end
+
+# 2)
+# * names is an array
+# names.each do |name|
+#   puts name.length
+# end
+
+# 3)
+# * numbers is an array
+# sum = 0
+# numbers.each do |number|
+#   sum += number
+# end
+# puts sum
+
+# 4)
+# * hash is a hash
+# hash.each do |name, age|
+#   puts "#{name} is #{age} years old."
+# end
+
+# 5)
+# * account is a hash
+# sum = 0
+# account.each do |transaction, value|
+#   sum += value
+# end
+# puts "The value the account is #{sum}"
+
+# 6)
+# * addresses is a hash
+# addresses.each do |name, address|
+#   puts "#{name} lives on #{address}"
+# end
+
+#  Write English<->Ruby answers here as comments in your code
+
+# 1. `For every element 'word' in the array 'sentences' print out the word.`
+
+# 2. `For every element 'phone_number' in the array 'numbers' print out the phone number if it is a MA area code.`
+
+# 3. `For every element number in the array 'numbers' print out every odd number.`
+
+# 4. `For every name-age pair in the hash 'ages', print out each pair.`
+
+# 5. `For every name-age pair in the hash 'ages', print out a pair if the age is > 10.`
+
+# 6. `For every name-age pair in the hash 'ages', print out a pair if the age is even.`
+
+
+# Write Ruby code to find out the answers to the following questions:
+array = [28214, 63061, 49928, 98565, 31769, 42316, 23674, 3540, 34953, 70282, 22077, 94710, 50353, 17107, 73683, 33287, 44575, 83602, 33350, 46583]
+
+# * What is the sum of all the numbers in `array`?
+# * How would you print out each value of the array?
+# * What is the sum of all of the even numbers?
+# * What is the sum of all of the odd numbers?
+# * What is the sum of all the numbers divisble by 5?
+# * What is the sum of the squares of all the numbers in the array?
+
+
+
+
+# Write Ruby code to find out the answers to the following questions:
+
+array = ["joanie", "annamarie", "muriel", "drew", "reva", "belle", "amari", "aida", "kaylie", "monserrate", "jovan", "elian", "stuart", "maximo", "dennis", "zakary", "louvenia", "lew", "crawford", "caitlyn"]
+
+# * How would you print out each name backwards in `array`?
+# * What are the total number of characters in the names in `array`?
+# * How many names in `array` are less than 5 characters long?
+# * How many names in `array` end in a vowel?
+# * How many names in `array` are more than 5 characters long?
+# * How many names in `array` are exactly 5 characters in length?
