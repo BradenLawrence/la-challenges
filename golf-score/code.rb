287682945f38d60f05a92a191fa7b739b58a2d51
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-13 09:38:55 -0500

    add display announcing the user's score relative to par

--- golf-score/code.rb
+++ golf-score/code.rb
@@ -19,12 +19,24 @@ scorecard = [
   {par: 5, strokes: 6}
 ]
 
-# Total Strokes: 81
-# Total Par: 72
-# You were 9 over par.
-
 total_strokes = scorecard.reduce(0) {|total, hole| total + hole[:strokes]}
 puts "Total Strokes: #{total_strokes}"
 
 total_par = scorecard.reduce(0) {|total, hole| total + hole[:par]}
 puts "Total Par: #{total_par}"
+
+score = total_strokes - total_par
+results = {
+  "a double eagle": -3,
+  "an eagle":       -2,
+  "a birdie":       -1,
+  "a par score":     0,
+  "a bogey":         1,
+  "a double bogey":  2,
+  "a triple bogey":  3
+}
+if results.values.include? score
+  puts "You hit #{results.reverse[score]}!"
+else
+  puts "You hit #{score.abs} #{score > 0 ? 'over' : 'under'} par."
+end
