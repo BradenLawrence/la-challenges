00f10eb0a207be7a20bc082f4e9fe43e3b8f5bf2
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-13 10:16:28 -0500

    add voter data as an array of hashes

---
+++ voter-demographics/code.rb
@@ -0,0 +1,94 @@
+# Francis, Underwood, 52, 100000, 2, Male, College
+
+voters = [
+  {
+    firstName: "Jon",
+    lastName:  "Smith",
+    age:       25,
+    income:    50000,
+    household: 1,
+    gender:    "Male",
+    education: "College"
+  },
+  {
+    firstName: "Jane",
+    lastName:  "Davies",
+    age:       30,
+    income:    60000,
+    household: 3,
+    gender:    "Female",
+    education: "High School"
+  },
+  {
+    firstName: "Sam",
+    lastName:  "Farelly",
+    age:       32,
+    income:    80000,
+    household: 2,
+    gender:    "Unspecified",
+    education: "College"
+  },
+  {
+    firstName: "Joan",
+    lastName:  "Favreau",
+    age:       35,
+    income:    65000,
+    household: 4,
+    gender:    "Female",
+    education: "College"
+  },
+  {
+    firstName: "Sam",
+    lastName:  "McNulty",
+    age:       38,
+    income:    63000,
+    household: 3,
+    gender:    "Male",
+    education: "College"
+  },
+  {
+    firstName: "Mark",
+    lastName:  "Minahan",
+    age:       48,
+    income:    78000,
+    household: 5,
+    gender:    "Male",
+    education: "High School"
+  },
+  {
+    firstName: "Susan",
+    lastName:  "Umani",
+    age:       45,
+    income:    75000,
+    household: 2,
+    gender:    "Female",
+    education: "College"
+  },
+  {
+    firstName: "Bill",
+    lastName:  "Perault",
+    age:       24,
+    income:    45000,
+    household: 1,
+    gender:    "Male",
+    education: "Did Not Complete High School"
+  },
+  {
+    firstName: "Doug",
+    lastName:  "Stamper",
+    age:       45,
+    income:    75000,
+    household: 1,
+    gender:    "Male",
+    education: "College"
+  },
+  {
+    firstName: "Francis",
+    lastName:  "Underwood",
+    age:       52,
+    income:    100000,
+    household: 2,
+    gender:    "Male",
+    education: "College"
+  }
+]
