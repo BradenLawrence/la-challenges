fb2aeb4af9c9167855c5b312a09e3ff8dc390635
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-14 11:58:02 -0500

    move question number printouts before logic

--- ballot-results/code.rb
+++ ballot-results/code.rb
@@ -30,6 +30,7 @@ vote_results.each do |precinct, tallies|
   end
 end
 
+puts "2)"
 precinct_totals = {
   "Precinct 1" => 0,
   "Precinct 2" => 0,
@@ -39,18 +40,18 @@ precinct_totals = {
 vote_results.each do |precinct, tallies|
   tallies.each {|candidate, votes| precinct_totals[precinct] += votes}
 end
-puts "2)"
 precinct_totals.each do |precinct, total|
   puts "#{precinct} had #{total} votes."
 end
 
+puts "3)"
 turnout_groups = precinct_totals.group_by {|precinct, total| total}
 highest_turnout = turnout_groups.max.last.to_h
-puts "3)"
 puts %W(#{highest_turnout.keys.join(" and ")}
         #{highest_turnout.size > 1 ? "are tied for most" : "had the most"}
         votes with #{highest_turnout.values[0]} votes.).join(" ")
 
+puts "4)"
 candidate_totals = {
   "Mary Sue" => 0,
   "Sally Jane" => 0,
@@ -59,32 +60,31 @@ candidate_totals = {
 vote_results.each do |precinct, tallies|
   tallies.each {|candidate, votes| candidate_totals[candidate] += votes}
 end
-puts "4)"
 candidate_totals.each do |candidate, total|
   puts "#{candidate} had #{total} votes"
 end
 
+puts "5)"
 precinct_query = "Precinct 4"
 query_groups = vote_results[precinct_query].group_by {|candidate, votes| votes}
 query_winner = query_groups.max.last.to_h
 query_winner_string = query_winner.keys.join(" and ")
-puts "5)"
 puts %W(#{query_winner_string} #{query_winner.size > 1 ? "are tied in" : "won"}
         #{precinct_query} with #{query_winner.values[0]} votes.).join(" ")
 
-total_votes = precinct_totals.values.reduce(:+)
 puts "6)"
+total_votes = precinct_totals.values.reduce(:+)
 puts "In total, #{total_votes} people voted."
 
+puts "7)"
 result_groups = candidate_totals.group_by {|candidate, totals| totals}
 winner = result_groups.max.last.to_h
 winner_string = winner.keys.join(" and ")
-puts "7)"
 puts %W(#{winner_string} #{winner.size > 1 ? "are tied" : "had the most votes"}
         with #{winner.values[0]} votes.).join(" ")
 
+puts "8)"
 last_place = result_groups.min.last.to_h
 difference = winner.values[0] - last_place.values[0]
 last_place_string = last_place.keys.join(" and ")
-puts "8)"
 puts "#{winner_string} beat #{last_place_string} by #{difference} votes."
