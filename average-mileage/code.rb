e826e860380f9afb35cfe55bf321a7a9b070a716
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-19 13:56:19 -0500

    add logic to average an input array

--- average-mileage/code.rb
+++ average-mileage/code.rb
@@ -2,6 +2,15 @@ shuvver_ship  = [260, 483, 792, 357, 546]
 zhook_cruiser = [186, 223, 173, 297, 303]
 nanoship      = [646, 883, 761, 932, 778]
 
-def average()
-
+def average(mileage_records)
+  return mileage_records.reduce(:+) / mileage_records.size.to_f
 end
+
+puts "Shuvver Ship:"
+puts average(shuvver_ship)
+puts ""
+puts "Zhook Cruiser:"
+puts average(zhook_cruiser)
+puts""
+puts "Nanoship"
+puts average(nanoship)
