2596c49f5008388b91777d4a14f52d9f7752e802
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-19 17:27:36 -0500

    add mileage ratings

--- average-mileage/code.rb
+++ average-mileage/code.rb
@@ -6,11 +6,29 @@ def average(mileage_records)
   return mileage_records.reduce(:+) / mileage_records.size.to_f
 end
 
+def mileage_rating(mileage)
+  categories = {
+    1000  =>  "A",
+     750  =>  "B",
+     500  =>  "C",
+     250  =>  "D",
+       0  =>  "F - Use this ship as a last resort! We aren\'t made of money!"
+  }
+  rating = categories.keys.find {|key| key < mileage}
+  return categories[rating]
+end
+
 puts "Shuvver Ship:"
-puts average(shuvver_ship)
+shuvver_average = average(shuvver_ship)
+puts "Average: #{shuvver_average}"
+puts "Rating: #{mileage_rating(shuvver_average)}"
 puts ""
 puts "Zhook Cruiser:"
-puts average(zhook_cruiser)
-puts""
+zhook_average = average(zhook_cruiser)
+puts "Average: #{zhook_average}"
+puts "Rating: #{mileage_rating(zhook_average)}"
+puts ""
 puts "Nanoship"
-puts average(nanoship)
+nanoship_average = average(nanoship)
+puts "Average: #{nanoship_average}"
+puts "Rating: #{mileage_rating(nanoship_average)}"
