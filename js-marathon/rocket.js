84a2f6585b7ba03ebcdabe34f2a384f818d64dd8
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-26 14:44:03 -0500

    add fire method

--- js-marathon/rocket.js
+++ js-marathon/rocket.js
@@ -7,6 +7,17 @@ let rocket = {
     } else {
       return "Sorry, the amount of fuel must be a positive number."
     }
+  },
+  fire() {
+    if(fuel > 0) {
+      fuel -= 1
+      console.log("Engines have fired!")
+      console.log(`Remaining fuel: ${fuel}`)
+      return true
+    } else {
+      console.log("Engines have failed to fire.")
+      return false
+    }
   }
 }
 
