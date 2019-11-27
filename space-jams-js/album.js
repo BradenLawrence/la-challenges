5655ae0b7c79dd210df5e80e68afd89df431d6c1
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-26 20:34:40 -0500

    add properties to Album class with summary and updateDuration methods

--- space-jams-js/album.js
+++ space-jams-js/album.js
@@ -1,5 +1,29 @@
 class Album {
-  //your code here
+  constructor(id, name, artists) {
+    this.id = id
+    this.name = name
+    this.artists = artists
+    this.tracks = []
+    this.durationMin = 0
+  }
+  updateDuration = () => {
+    let durationMil = this.tracks.reduce((total, track) => {
+
+      return total += parseInt(track.duration_ms)
+    }, 0)
+    this.durationMin = (durationMil / 60000).toFixed(2)
+  }
+  summary = () => {
+    this.updateDuration()
+    let summaryString = (`Name: ${this.name}
+Artist(s): ${this.artists}
+Duration (min.): ${this.durationMin}
+Tracks:\n`)
+    this.tracks.forEach(track => {
+      summaryString += `- ${track.title}\n`
+    })
+    return summaryString
+  }
 }
 
 module.exports = Album;
