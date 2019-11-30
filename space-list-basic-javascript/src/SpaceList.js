b45f46a4db8224c8ec6a87b8ed9aa93691301d56
Braden Lawrence <lawrencebradenm@gmail.com>
2019-11-30 16:40:20 -0500

    add constructor for SpaceList and checkInventory method

--- space-list-basic-javascript/src/SpaceList.js
+++ space-list-basic-javascript/src/SpaceList.js
@@ -1,5 +1,25 @@
 class SpaceList {
-  constructor(name, SpaceItems) {
-    
+  constructor(name, SpaceItems = []) {
+    this.name = name
+    this.items = SpaceItems
+  }
+  checkInventory = (inventory) => {
+    let stillNeeded = {}
+    this.items.forEach(item => {
+      let itemDeficit = item.quantity - inventory[item.name]
+      if(itemDeficit > 0) {
+        stillNeeded[item.name] = itemDeficit
+      }
+    })
+    if(Object.keys(stillNeeded).length === 0) {
+      return "The lab has it all! I got you everything on your list here."
+    } else {
+      let report = "Sorry, the lab doesn't have enough of the following:\n"
+      for(let item in stillNeeded) {
+        report += `- ${item}\n`
+      }
+      report += "You'll have to go someplace else to get these."
+      return report
+    }
   }
 }
