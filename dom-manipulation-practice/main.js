c6b9a644be78d624b56cf5479e37daa2917c82e6
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-02 16:42:17 -0500

    add new todo items to the <ul> dom element

--- dom-manipulation-practice/main.js
+++ dom-manipulation-practice/main.js
@@ -1,1 +1,9 @@
-// Your code goes here!
+let todos = [
+  "Write some JavaScript",
+  "Submit System Check",
+  "Take a Break"
+]
+
+todos.forEach(todo => {
+  document.getElementById("todo-list").innerHTML += `<li>${todo}</li>`
+})
