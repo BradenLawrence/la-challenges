0ecc23ba0ba8438d0d0b5f25f359067a46b1af55
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-03 16:20:50 -0500

    add tutorial code

--- javascript-events-and-event-handlers/main.js
+++ javascript-events-and-event-handlers/main.js
@@ -1,1 +1,14 @@
-// Your code goes here!
+let text = document.getElementById('changing-color-text')
+let changeFontColor = (event) => {
+  let colors = ['Aqua', 'Blue', 'Orange', 'Green', 'Red', 'LightSteelBlue']
+  let index = Math.floor(Math.random() * colors.length)
+  let color = colors[index]
+
+  event.target.style.color = color
+}
+
+text.addEventListener('click', changeFontColor)
+
+window.onload = () => {
+  console.log('window loaded.')
+}
