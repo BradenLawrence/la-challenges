9f1290424710201c4706042616f9dc4175b0d014
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-06 20:47:10 -0500

    add random quote generator

--- launcher-resume-rebuild/main.js
+++ launcher-resume-rebuild/main.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+
+let quoteBtn = document.querySelector('#quote')
+let quoteDisplay = document.querySelector('#quote-display')
+let quoteList =  [
+  "A good programmer is someone who looks both ways before crossing a one-way street.",
+  "Java is to JavaScript as ham is to hamster.",
+  "How do you comfort a JavaScript bug? You console it."
+]
+quoteBtn.addEventListener('click', event => {
+  let randomQuote = quoteList[Math.floor(Math.random()*quoteList.length)]
+  quoteDisplay.innerHTML = randomQuote
+})
