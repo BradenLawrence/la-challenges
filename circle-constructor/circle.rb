cd1af4329eebd3f682ea3fe896e25d04d909dffb
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-09 15:52:13 -0500

    update initializer to handle radius or diameter parameters

--- circle-constructor/circle.rb
+++ circle-constructor/circle.rb
@@ -1,5 +1,15 @@
+# class Circle
+#   def initialize(radius)
+#     @radius = radius
+#   end
+# end
+
 class Circle
-  def initialize(radius)
-    @radius = radius
+  def initialize(dimensions)
+    if dimensions.has_key?(:radius)
+      @radius = dimensions[:radius]
+    elsif dimensions.has_key?(:diameter)
+      @radius = dimensions[:diameter] / 2
+    end
   end
 end
