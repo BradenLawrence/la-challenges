3ce56b2b1e77e8200363fc6fd143692d60955f73
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-09 15:05:40 -0500

    add rescue condition for nil last_name

--- person-constructor/person.rb
+++ person-constructor/person.rb
@@ -1,6 +1,12 @@
 class Person
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(first_name, last_name = nil)
+    if last_name.nil?
+      full_name = first_name.split
+      @first_name = full_name.first
+      @last_name = full_name.last
+    else
+      @first_name = first_name
+      @last_name = last_name
+    end
   end
 end
