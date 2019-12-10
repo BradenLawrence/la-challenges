d3e73abc0db59220a9cb7976f2295bf95faea3cc
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 15:03:37 -0500

    add total hand value method

--- high-card-dealer/lib/hand.rb
+++ high-card-dealer/lib/hand.rb
@@ -2,6 +2,10 @@ class Hand
   def initialize(cards)
     @cards = cards
   end
+
+  def value
+    @cards.reduce(0) {|total, card| total += card.value}
+  end
 end
 # your code here
 # Initialize with an array of card objects
