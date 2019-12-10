f76bab8e01d7869577803391345d28ac4c051feb
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 08:59:07 -0500

    update song list to be an array constant

--- the-jukebox/jukebox.rb
+++ the-jukebox/jukebox.rb
@@ -1,21 +1,24 @@
 class Jukebox
+  attr_read DISCOGRAPHY
+
+  DISCOGRAPHY = [
+    "Hello - Lionel Ritchie",
+    "Kokomo - The Beach Boys",
+    "Girl You Know It’s True - Milli Vanilli",
+    "Agadoo - Black Lace",
+    "Down Under - Men at Work",
+    "Nothing's Gonna Stop Us Now - Starship",
+    "Get Outta My Dreams, Get Into My Car - Billy Ocean",
+    "I Just Called To Say I Love You - Stevie Wonder",
+    "Hangin' Tough - New Kids on the Block",
+    "We Built This City - Starship",
+    "Wake Me Up Before You Go Go - Wham!",
+    "We Didn't Start The Fire - Billy Joel",
+    "I Wanna Dance With Somebody - Whitney Houston",
+    "U Can't Touch This - MC Hammer"
+  ]
+
   def initialize(request)
     @request = request
-    @discography = {
-      "Hello" => "Lionel Ritchie",
-      "Kokomo" => "The Beach Boys",
-      "Girl You Know It’s True" => "Milli Vanilli",
-      "Agadoo" => "Black Lace",
-      "Down Under" => "Men at Work",
-      "Nothing's Gonna Stop Us Now" => "Starship",
-      "Get Outta My Dreams, Get Into My Car" => "Billy Ocean",
-      "I Just Called To Say I Love You" => "Stevie Wonder",
-      "Hangin' Tough" => "New Kids on the Block",
-      "We Built This City" => "Starship",
-      "Wake Me Up Before You Go Go" => "Wham!",
-      "We Didn't Start The Fire" => "Billy Joel",
-      "I Wanna Dance With Somebody" => "Whitney Houston",
-      "U Can't Touch This" => "MC Hammer"
-    }
   end
 end
