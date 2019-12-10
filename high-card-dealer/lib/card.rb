3770039aa0373401386c316ac6b9ba342163b410
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 14:53:53 -0500

    update card value to handle face cards

--- high-card-dealer/lib/card.rb
+++ high-card-dealer/lib/card.rb
@@ -1,11 +1,13 @@
 class Card
+  RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
   end
 
   def value
-    @rank
+    RANKS[@rank].index + 1
   end
   # Methods you may need:
   # - initialize
