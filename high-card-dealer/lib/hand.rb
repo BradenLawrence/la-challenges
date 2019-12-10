5f48003c77f7e4e1f8bdfd80d306f71279d22879
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 14:40:33 -0500

    add hand class which takes an array of Card objects

--- high-card-dealer/lib/hand.rb
+++ high-card-dealer/lib/hand.rb
@@ -1,1 +1,10 @@
+class Hand
+  def initialize(cards)
+    @cards = cards
+  end
+end
 # your code here
+# Initialize with an array of card objects
+# method that returns total value of cards
+# Players represented by instances of hand
+# Limit of 4 cards
