41ae0263337545c6a03a5fb94b2175764e92ff9e
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 17:13:43 -0500

    add getters for rank and suit

--- high-card-dealer/lib/card.rb
+++ high-card-dealer/lib/card.rb
@@ -1,4 +1,5 @@
 class Card
+  attr_reader :rank, :suit
   RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 
   def initialize(rank, suit)
@@ -9,9 +10,4 @@ class Card
   def value
     RANKS.index(@rank) + 1
   end
-  # Methods you may need:
-  # - initialize
-  # - method that assigns a card its "value"
-  # - attribute readers
-  # - any other methods you deem necessary
 end
