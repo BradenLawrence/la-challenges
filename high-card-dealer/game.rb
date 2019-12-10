25566cfdcab2a4f052ba57b884ea89d587d60c7f
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 13:35:58 -0500

    start high-card-dealer challenge

---
+++ high-card-dealer/game.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+# Note: we only need `require_relative` if we end up calling a class by name in our file's logic. As such, you may have to add more `require_relative` statements accordingly.
+require_relative "lib/deck"
+
+puts 'GAME START'
+
+# Your game logic here.
---
+++ high-card-dealer/lib/card.rb
@@ -0,0 +1,9 @@
+class Card
+  # Your code here
+
+  # Methods you may need:
+  # - initialize
+  # - method that assigns a card its "value"
+  # - attribute readers
+  # - any other methods you deem necessary
+end
