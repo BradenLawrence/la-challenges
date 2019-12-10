4befc7d6ab4488581c760e9ee05201caeb5f26c8
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-09 22:30:48 -0500

    add list of available songs

--- the-jukebox/jukebox.rb
+++ the-jukebox/jukebox.rb
@@ -1,5 +1,21 @@
 class Jukebox
   def initialize(playlist)
     @playlist = playlist
+    @discography = {
+      "Hello" => "Lionel Ritchie",
+      "Kokomo" => "The Beach Boys",
+      "Girl You Know It’s True" => "Milli Vanilli",
+      "Agadoo" => "Black Lace",
+      "Down Under" => "Men at Work",
+      "Nothing's Gonna Stop Us Now" => "Starship",
+      "Get Outta My Dreams, Get Into My Car" => "Billy Ocean",
+      "I Just Called To Say I Love You" => "Stevie Wonder",
+      "Hangin' Tough" => "New Kids on the Block",
+      "We Built This City" => "Starship",
+      "Wake Me Up Before You Go Go" => "Wham!",
+      "We Didn't Start The Fire" => "Billy Joel",
+      "I Wanna Dance With Somebody" => "Whitney Houston",
+      "U Can't Touch This" => "MC Hammer"
+    }
   end
 end
