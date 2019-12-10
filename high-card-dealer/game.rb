f6e01462edec7e2fbce4e99837a70e6d38981a02
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-10 14:43:22 -0500

    add hand class and deal two hands of four cards

--- high-card-dealer/game.rb
+++ high-card-dealer/game.rb
@@ -2,7 +2,19 @@ require 'pry'
 
 # Note: we only need `require_relative` if we end up calling a class by name in our file's logic. As such, you may have to add more `require_relative` statements accordingly.
 require_relative "lib/deck"
+require_relative "lib/hand"
 
 puts 'GAME START'
 
+deck = Deck.new
+
+hand1 = Hand.new(deck.deal(4))
+hand2 = Hand.new(deck.deal(4))
+
+binding.pry
+
 # Your game logic here.
+# Shift 4 cards into a new Hand object 2 times
+# Define parameters for number of cards dealt and number of players
+# Compare total values of hand objects
+# Decide winner!
