1127386445cb17630c2236842a934fed22b47355
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-11 13:57:04 -0500

    add methods for fill_with_gredient, weight, and how_many_ingredients

--- emergency-brussels-sprouts/lib/container.rb
+++ emergency-brussels-sprouts/lib/container.rb
@@ -1,8 +1,30 @@
 class Container
-  attr_reader :weight, :maximum_holding_weight
+  attr_reader :dry_weight, :current_holding_weight, :maximum_holding_weight, :ingredients, :which_ingredient
 
   def initialize(weight, maximum_holding_weight)
+    @dry_weight = weight
     @weight = weight
     @maximum_holding_weight = maximum_holding_weight
+    @current_holding_weight = 0
+    @ingredients = []
+    @which_ingredient = nil
+  end
+
+  def fill_with_ingredient(ingredient)
+    quantity = (@maximum_holding_weight / ingredient.weight).to_i
+    @which_ingredient = ingredient.name
+    quantity.times do
+      @ingredients << ingredient
+      @current_holding_weight += ingredient.weight
+    end
+    @current_holding_weight = @current_holding_weight.ceil
+  end
+
+  def weight
+    @dry_weight + current_holding_weight
+  end
+
+  def how_many_ingredients
+    @ingredients.size
   end
 end
