c3ee933920d4c04f432b5e4e6a4dc301a99b9bed
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-12 09:32:52 -0500

    add circle class with diameter, area, and perimeter methods

--- geometric-classes/lib/circle.rb
+++ geometric-classes/lib/circle.rb
@@ -1,2 +1,21 @@
 class Circle
+  attr_reader :radius, :x, :y
+
+  def initialize(radius, x = 0 ,y = 0)
+    @radius = radius
+    @x = x
+    @y = y
+  end
+
+  def diameter
+    @radius * 2
+  end
+
+  def area
+    Math::PI * @radius * @radius
+  end
+
+  def perimeter
+    2 * Math::PI * @radius
+  end
 end
