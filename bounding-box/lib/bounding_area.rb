4e9489a9065f3ecf8f3b38183206f7df62769c28
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-11 22:48:33 -0500

    add bounding area class, which contains multiple bounding boxes

--- bounding-box/lib/bounding_area.rb
+++ bounding-box/lib/bounding_area.rb
@@ -1,3 +1,17 @@
 class BoundingArea
-  # your code here
+  attr_reader :bounding_boxes
+
+  def initialize(bounding_boxes)
+    @bounding_boxes = bounding_boxes
+  end
+
+  def boxes_contain_point?(x, y)
+    @bounding_boxes.each do |box|
+      if box.contains_point?(x, y)
+        return true
+      end
+    end
+    return false
+  end
+
 end
