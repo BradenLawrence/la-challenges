0169b11a8d1e86847538970fd511ab3e0eefb5ce
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-16 13:05:48 -0500

    start magical-creatures challenge

---
+++ magical-creatures/server.rb
@@ -0,0 +1,31 @@
+require "sinatra"
+require "pry" if development? || test?
+require "sinatra/reloader" if development?
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+# The MAGICAL_CREATURES variable is an array of hashes, and can be accessed from an of your routes.
+MAGICAL_CREATURES = [
+  {
+    name: "unicorn",
+    ability: "Its horn is said to have the power to render poisoned water potable and heal sickness.",
+    age: 140
+  },
+  {
+    name: "dragon",
+    ability: "Breathes fire and has a hide of scales that is near impenetrable.",
+    age: 587
+  },
+  {
+    name: "phoenix",
+    ability: "When it is old and weary, its body ignites in flame and it arises anew from the ashes of its predecessor.",
+    age: nil
+  }
+]
+
+# You may define your routes here. Start with the root path, and designate an appropriately named erb template!
+
+# Example:
+# get "/wizards" do
+#   erb :wizards
+# end
---
+++ magical-creatures/spec/models/magical_creature_spec.rb
@@ -0,0 +1,8 @@
+# FOR NON-CORE STORIES ONLY
+
+require "spec_helper"
+# require_relative "../models/magical_creature.rb"
+
+# RSpec.describe MagicalCreature do
+
+# end
