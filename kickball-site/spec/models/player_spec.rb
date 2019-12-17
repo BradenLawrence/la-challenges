c01625cd945b8c22f52e20a3e85baeac11d870c9
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-16 22:27:33 -0500

    add tests for Player class

--- kickball-site/spec/models/player_spec.rb
+++ kickball-site/spec/models/player_spec.rb
@@ -1,15 +1,17 @@
 require "spec_helper"
 
+# Player objects should be initialized with a name and position. These attributes should have reader methods associated with them.
+
 RSpec.describe Player do
   let(:player_name) { 'Count Chocula' }
   let(:position) { 'Pitcher' }
   let(:player) { Player.new(player_name, position) }
 
   it 'has a name' do
-    # your code here
+    expect(:player.name).to eq('Count Chocula')
   end
 
   it 'has a position' do
-    # your code here
+    expect(:player.position).to eq('Pitcher')
   end
 end
