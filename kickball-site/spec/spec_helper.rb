006673bb8b39c98af1660fb2da77d84bb8718fb2
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-16 23:23:15 -0500

    add classes to spec helper

--- kickball-site/spec/spec_helper.rb
+++ kickball-site/spec/spec_helper.rb
@@ -3,5 +3,8 @@ require "capybara"
 require "capybara/rspec"
 
 require_relative "../server"
+require_relative "../models/team"
+require_relative "../models/player"
+require_relative "../models/league"
 
 Capybara.app = Sinatra::Application
