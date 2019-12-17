e193034533029e2b13e47851d4986af62c60814a
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-16 23:37:37 -0500

    remove comment

--- kickball-site/spec/features/teams_spec.rb
+++ kickball-site/spec/features/teams_spec.rb
@@ -6,8 +6,6 @@ feature "view teams index" do
   # I want to view a page with the names of each team
   # So that I can learn which teams are in the league
 
-  # * [ ] When I visit /teams I should be able to see list of all the team names
-
   scenario "user visits /teams path" do
     visit "/teams"
     expect(page).to have_content("Simpson Slammers")
