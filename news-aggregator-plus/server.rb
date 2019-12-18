ece2b64629028f7a5050534b5c137a332bb931ec
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-18 15:41:30 -0500

    add show route

--- news-aggregator-plus/server.rb
+++ news-aggregator-plus/server.rb
@@ -7,24 +7,50 @@ require_relative 'models/article'
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 
-get "/" do
-  redirect "/articles"
-end
-
-get "/articles" do
+def render_articles
   csv_rows = CSV.readlines("articles.csv", headers: true)
-  @articles = []
+  articles = []
   csv_rows.each_with_index do |row, index|
-    @articles << Article.new(
+    articles << Article.new(
       row["title"],
       row["description"],
       row["URL"],
       index
     )
   end
+  return articles
+end
+
+get "/" do
+  redirect "/articles"
+end
+
+get "/articles" do
+  @articles = render_articles
   erb :articles
 end
 
-get "/articles/:name" do
+post "/articles" do
+  title = params["title"]
+  description = params["description"]
+  url = params["url"]
+  CSV.open("articles.csv", "a") do |csv|
+    csv << [description, title, url]
+  end
+  redirect "/articles"
+end
+
+get "/articles/new" do
+  erb :new
+end
+
+get "/articles/:article_id" do
+  @id = params["article_id"].to_i
+  articles = render_articles
+  @found_article = articles.find do |article|
+    article.id == @id
+  end
+  erb :show
+end
 
 end
