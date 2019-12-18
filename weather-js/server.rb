c489392e09df571bb3d2cb308b8c64dc2274aedf
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-18 09:21:31 -0500

    refactor index.html into a layout and template erb

--- weather-js/server.rb
+++ weather-js/server.rb
@@ -10,7 +10,7 @@ set :bind, '0.0.0.0'  # bind to all interfaces
 DARK_SKY_API_KEY = "2463e9d833037e62a0d2facda247c163"
 
 get "/" do
-  File.read("public/index.html")
+  erb :index
 end
 
 get "/api/v1/forecast/:latitude,:longitude" do |latitude, longitude|
