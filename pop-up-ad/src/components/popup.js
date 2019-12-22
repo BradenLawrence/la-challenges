b61126ce9709355a74f83e26f02740f15a6c2887
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-22 16:33:16 -0500

    refactor Popup into its own component and add click handler

---
+++ pop-up-ad/src/components/popup.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Popup = props => {
+  const handleExit = event => {
+    window.confirm("Are you sure you want to give up your Tay Tay tickets?")
+  }
+
+  return(
+    <div className="callout" data-closable="slide-out-right">
+      <h5 className="text-center">You have won tickets to see Taylor Swift!!</h5>
+      <p className="text-center">
+        Please enter your email so we can send you the tickets
+      </p>
+      <button
+        onClick={handleExit}
+        className="close-button"
+        aria-label="Dismiss alert"
+        type="button"
+        data-close>
+        <span aria-hidden="true"><i className="fas fa-times"></i></span>
+      </button>
+      <div className="input-group">
+        <span className="input-group-label">Email</span>
+        <input className="input-group-field" type="number"></input>
+        <div className="input-group-button">
+          <input type="submit" className="button" value="Submit"></input>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Popup
--- pop-up-ad/src/main.js
+++ pop-up-ad/src/main.js
@@ -1,25 +1,7 @@
 import './app.scss';
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Popup = props => {
-  return(
-    <div className="callout" data-closable="slide-out-right">
-      <h5 className="text-center">You have won tickets to see Taylor Swift!!</h5>
-      <p className="text-center">Please enter your email so we can send you the tickets</p>
-      <button className="close-button" aria-label="Dismiss alert" type="button" data-close>
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <div className="input-group">
-        <span className="input-group-label">Email</span>
-        <input className="input-group-field" type="number"></input>
-        <div className="input-group-button">
-          <input type="submit" className="button" value="Submit"></input>
-        </div>
-      </div>
-    </div>
-  )
-}
+import Popup from './components/popup'
 
 const App = () => {
   return(
