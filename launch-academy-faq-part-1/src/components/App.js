39ee7c4962de1e32b8649605038f86d74f0ed2f8
Braden Lawrence <lawrencebradenm@gmail.com>
2019-12-31 15:06:31 -0500

    add show/hide click handler for the answer text

--- launch-academy-faq-part-1/src/components/App.js
+++ launch-academy-faq-part-1/src/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionTile from './QuestionTile'
 
 const App = (props) => {
+  const [visibility, setVisibility] = useState(null)
+
+  const visibilityToggle = () => {
+    if(visibility === null) {
+      setVisibility(1)
+    } else {
+      setVisibility(null)
+    }
+  }
+
   return(
     <QuestionTile
       question={props.data.question}
       answer={props.data.answer}
+      clickHandler={visibilityToggle}
+      className={visibility === 1 ? "visible" : "hidden"}
     />
   )
 }
--- launch-academy-faq-part-1/src/components/QuestionTile.js
+++ launch-academy-faq-part-1/src/components/QuestionTile.js
@@ -3,11 +3,13 @@ import React from 'react'
 const QuestionTile = props => {
   return(
     <div>
-      <input type="button" value="+"/>
+      <input onClick={props.clickHandler} type="button" value="+"/>
       <p><strong>Question</strong></p>
       <p>{props.question}</p>
-      <p><strong>Answer</strong></p>
-      <p>{props.answer}</p>
+      <div className={props.className}>
+        <p><strong>Answer</strong></p>
+        <p>{props.answer}</p>
+      </div>
     </div>
   )
 }
