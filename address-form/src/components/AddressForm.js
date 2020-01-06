c70428b367a5bbedc6cd706fd6d2f6093ee9802c
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-06 08:48:32 -0500

    add state control to forms and submithandler

--- address-form/src/components/AddressForm.js
+++ address-form/src/components/AddressForm.js
@@ -1,45 +1,123 @@
-import React from "react"
+import React, { useState } from "react"
 
 const AddressForm = props => {
+  const [address, setAddress] = useState({
+    firstName: "",
+    lastName: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    phoneNumber: "",
+    email: ""
+  })
+
+  const handleInputChange = (event) => {
+    let input = event.currentTarget.id
+    let value = event.currentTarget.value
+    setAddress({
+      ...address,
+      [input]: value
+    })
+    console.log(value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(address)
+  }
+
   return (
-    <form className="callout" id="shipping-address-form">
+    <form
+      className="callout"
+      id="shipping-address-form"
+      onSubmit={handleSubmit}
+    >
       <h1>Shipping Address</h1>
       <div>
         <label htmlFor="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" />
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={address.firstName}
+          onChange={handleInputChange}
+          />
       </div>
 
       <div>
         <label htmlFor="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={address.lastName}
+          onChange={handleInputChange}
+          />
       </div>
 
       <label htmlFor="address">Address:</label>
-      <input type="text" id="address" name="address" />
+      <input
+        type="text"
+        id="street"
+        name="street"
+        value={address.street}
+        onChange={handleInputChange}
+        />
 
       <div>
         <label htmlFor="city">City:</label>
-        <input type="text" id="city" name="city" />
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={address.city}
+          onChange={handleInputChange}
+          />
       </div>
 
       <div>
         <label htmlFor="state">State:</label>
-        <input type="text" id="state" name="state" />
+        <input
+          type="text"
+          id="state"
+          name="state"
+          value={address.state}
+          onChange={handleInputChange}
+          />
       </div>
 
       <div>
         <label htmlFor="zipCode">Zip Code:</label>
-        <input type="text" id="zipCode" name="zipCode" />
+        <input
+          type="text"
+          id="zipCode"
+          name="zipCode"
+          value={address.zipCode}
+          onChange={handleInputChange}
+          />
       </div>
 
       <div>
         <label htmlFor="phoneNumber">Phone Number:</label>
-        <input type="text" id="phoneNumber" name="phoneNumber" />
+        <input
+          type="text"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={address.phoneNumber}
+          onChange={handleInputChange}
+          />
       </div>
 
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={address.email}
+          onChange={handleInputChange}
+          />
       </div>
 
       <input type="submit" className="button" value="Submit " />
