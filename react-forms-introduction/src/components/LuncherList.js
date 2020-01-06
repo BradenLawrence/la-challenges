89b438a9bff48cf7eae72b139bbdab04c2214e11
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-06 09:07:19 -0500

    remove debugger

--- react-forms-introduction/src/components/LuncherList.js
+++ react-forms-introduction/src/components/LuncherList.js
@@ -1,7 +1,6 @@
 import React from "react"
 const LuncherList = props => {
   const { lunchers } = props
-  debugger
   const listItems = lunchers.map(luncher => {
     return <li key={luncher}>{luncher}</li>
   })
