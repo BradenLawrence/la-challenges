d25830e415b70b0e5ea526e6366f731cf2a1114e
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-08 16:48:03 -0500

    add image property test and click mock test

--- testing-react-presentational-components-jest/src/components/Elephant.js
+++ testing-react-presentational-components-jest/src/components/Elephant.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Elephant = props => {
   return (
     <div onClick={props.onClick} className="center">
       <img src={props.image} height="400" width="600" />
       <h1>{props.text}</h1>
+      <hr />
+      <Link to="/facts">Check out some elephant facts</Link>
     </div>
   )
 }
--- testing-react-presentational-components-jest/src/components/Elephant.test.js
+++ testing-react-presentational-components-jest/src/components/Elephant.test.js
@@ -1,15 +1,23 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
 import Enzyme, { mount } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 Enzyme.configure({ adapter: new Adapter() })
 
 import Elephant from "./Elephant"
 
-let wrapper
+let wrapper, onClickMock
 
 beforeEach(() => {
+  onClickMock = jest.fn()
   wrapper = mount(
-    <Elephant image="http://fakeurl.com/elephant" text="I am an Elephant!" />
+    <BrowserRouter>
+      <Elephant
+        image="http://fakeurl.com/elephant"
+        text="I am an Elephant!"
+        onClick={onClickMock}
+      />
+    </BrowserRouter>
   )
 })
 
@@ -20,5 +28,15 @@ describe("Elephant", () => {
   it("Should have an H1 tag defined", () => {
     expect(wrapper.find('h1').text()).toBe('I am an Elephant!')
   })
-
+  it("Should render an image with specific props", () => {
+    expect(wrapper.find('img').props()).toEqual({
+      src: "http://fakeurl.com/elephant",
+      width: "600",
+      height: "400"
+    })
+  })
+  it("Should have an onClick function", () => {
+    wrapper.simulate('click')
+    expect(onClickMock).toHaveBeenCalled()
+  })
 })
