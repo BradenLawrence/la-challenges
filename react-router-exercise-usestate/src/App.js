cf714c458e0a0746eb898fdf552b000b08e08b84
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-07 22:35:38 -0500

    nest routes within nav component

--- react-router-exercise-usestate/src/App.js
+++ react-router-exercise-usestate/src/App.js
@@ -1,18 +1,12 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Greeting from "./components/Greeting"
-import Goodbye from "./components/Goodbye"
 import NavBar from "./components/NavBar"
-import CustomGreeting from "./components/CustomGreeting"
 
 const App = props => {
   return(
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={NavBar}/>
-        <Route exact path="/greeting" component={Greeting}/>
-        <Route exact path="/goodbye" component={Goodbye}/>
-        <Route exact path="/customGreeting" component={CustomGreeting}/>
+        <Route path="/" component={NavBar}/>
       </Switch>
     </BrowserRouter>
   )
--- react-router-exercise-usestate/src/components/NavBar.js
+++ react-router-exercise-usestate/src/components/NavBar.js
@@ -1,13 +1,23 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, Route, Switch } from "react-router-dom"
+import Greeting from "./Greeting"
+import Goodbye from "./Goodbye"
+import CustomGreeting from "./CustomGreeting"
 
 const NavBar = props => {
   return(
-    <ul>
-      <li><Link to="/greeting">Greeting</Link></li>
-      <li><Link to="/goodbye">Goodbye</Link></li>
-      <li><Link to="/customGreeting">Custom Greeting</Link></li>
-    </ul>
+    <div>
+      <ul>
+        <li><Link to="/greeting">Greeting</Link></li>
+        <li><Link to="/goodbye">Goodbye</Link></li>
+        <li><Link to="/customGreeting">Custom Greeting</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/greeting" component={Greeting}/>
+        <Route exact path="/goodbye" component={Goodbye}/>
+        <Route exact path="/customGreeting" component={CustomGreeting}/>
+      </Switch>
+    </div>
   )
 }
 
