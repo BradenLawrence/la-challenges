764d2d74fa855406114c99125e7c58289309c1be
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-07 22:27:43 -0500

    add routes for three pages and a nav page

--- react-router-exercise-usestate/src/components/NavBar.js
+++ react-router-exercise-usestate/src/components/NavBar.js
@@ -1,7 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const NavBar = props => {
-  return <h3>Create Links Here</h3>
+  return(
+    <ul>
+      <li><Link to="/greeting">Greeting</Link></li>
+      <li><Link to="/goodbye">Goodbye</Link></li>
+      <li><Link to="/customGreeting">Custom Greeting</Link></li>
+    </ul>
+  )
 }
 
 export default NavBar
