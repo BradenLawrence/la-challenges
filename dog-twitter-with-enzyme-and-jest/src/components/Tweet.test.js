969150e641d5d2305a63f1d77efb0628b98b470b
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-09 13:53:06 -0500

    add tests for tweet and twitterfeed

--- dog-twitter-with-enzyme-and-jest/src/components/Tweet.test.js
+++ dog-twitter-with-enzyme-and-jest/src/components/Tweet.test.js
@@ -7,6 +7,39 @@ import Tweet from "./Tweet"
 
 Enzyme.configure({ adapter: new Adapter() })
 
+
 describe("Tweet", () => {
-  // YOUR TEST CODE HERE
+  let fakeHandleClick = jest.fn()
+  let wrapper
+  let data = {
+    id: 1,
+    text: "This doesn't look like Kansas",
+    user: {
+      name: "Todo",
+      profile_image_url:
+        "http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg"
+    }
+  }
+  beforeEach(() => {
+    wrapper = mount(
+      <Tweet
+        key="1"
+        tweet={data}
+        handleClick={fakeHandleClick}
+        className=""
+      />
+    )
+  })
+
+  it('should render with the provided tweet text', () => {
+    expect(wrapper.find('p').text()).toBe("This doesn't look like Kansas")
+  })
+
+  it('should render with the provided tweet username', () => {
+    expect(wrapper.find('span').text()).toBe("Todo")
+  })
+
+  it('should render with the provided image', () => {
+    expect(wrapper.find('img').props()["src"]).toBe("http://images2.fanpop.com/image/quiz/400000/400752_1271413686448_431_300.jpg")
+  })
 })
