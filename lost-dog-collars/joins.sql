f80106be207a557f95a410dae938aa455f51916b
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-13 01:03:10 -0500

    add AS statements to update column headings

--- lost-dog-collars/joins.sql
+++ lost-dog-collars/joins.sql
@@ -1,71 +1,71 @@
 -- #1
 -- Which collars have a known owner?
 -- Display only collars with known owners and those owners' names.
-SELECT dog_owners.name, lost_dog_collars.dog_name
+SELECT dog_owners.name AS owner, lost_dog_collars.dog_name AS collars
   FROM dog_owners
   JOIN lost_dog_collars
   ON (dog_owners.dog_name = lost_dog_collars.dog_name);
 
 -- RESULT:
--- name    | dog_name
--- --------+----------
--- Evan    | Bogie
--- Whitney | Gilly
--- Spencer | Lilly
--- Dan     | Linux
--- Omid    | Bronson
+--   owner  | collars
+-- ---------+---------
+--  Evan    | Bogie
+--  Whitney | Gilly
+--  Spencer | Lilly
+--  Dan     | Linux
+--  Omid    | Bronson
 
 -- #2
 -- For which collars is there no known owner?
 -- Display only collars without known owners.
 
-SELECT dog_owners.name, lost_dog_collars.dog_name
-  FROM dog_owners
-  RIGHT JOIN lost_dog_collars
+SELECT lost_dog_collars.dog_name AS collars, dog_owners.name
+  FROM lost_dog_collars
+  LEFT JOIN dog_owners
   ON (dog_owners.dog_name = lost_dog_collars.dog_name)
   WHERE dog_owners.name IS NULL;
 
 -- RESULT:
--- name | dog_name
--- -----+----------
---      | Lassie
---      | Fido
---      | Goose
+--  collars | name
+-- ---------+------
+--  Lassie  |
+--  Fido    |
+--  Goose   |
 
 -- #3
 -- What collars are in our possession?
 -- Display all collars in our possession.
 -- If an owner exists for a given collar, display that also.
 
-SELECT lost_dog_collars.dog_name, dog_owners.name
+SELECT lost_dog_collars.dog_name AS collar, dog_owners.name AS owner
   FROM lost_dog_collars                                                                          LEFT JOIN dog_owners
   ON (lost_dog_collars.dog_name = dog_owners.dog_name);
 
 -- RESULT:
---  dog_name |  name
--- ----------+---------
---  Bogie    | Evan
---  Lassie   |
---  Gilly    | Whitney
---  Lilly    | Spencer
---  Fido     |
---  Linux    | Dan
---  Bronson  | Omid
---  Goose    |
+--  collar  |  owner
+-- ---------+---------
+--  Bogie   | Evan
+--  Lassie  |
+--  Gilly   | Whitney
+--  Lilly   | Spencer
+--  Fido    |
+--  Linux   | Dan
+--  Bronson | Omid
+--  Goose   |
 
 -- #4
 -- What owners do we know about?
 -- Display all owners known to us.
 -- If a collar matches that owner, display the collar also.
 
-SELECT dog_owners.name, lost_dog_collars.dog_name
+SELECT dog_owners.name AS owner, lost_dog_collars.dog_name as collar
   FROM dog_owners
   LEFT JOIN lost_dog_collars
   ON (lost_dog_collars.dog_name = dog_owners.dog_name);
 
 -- RESULT:
---   name   | dog_name
--- ---------+----------
+--   owner  | collar
+-- ---------+---------
 --  Evan    | Bogie
 --  Whitney | Gilly
 --  Spencer | Lilly
