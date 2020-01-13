dce1a7d2b308605a3a686d6b2036dcefad29cf04
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-12 22:25:32 -0500

    follow article and create routes to retrieve data from db

---
+++ sql-basics-with-postgres-and-ruby/server.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'pg'
+require 'pry'
+
+set :bind, '0.0.0.0'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'pet_db')
+    yield(connection)
+  ensure
+    connection.close()
+  end
+end
+
+get '/pets' do
+  @pets = db_connection {|conn| conn.exec('SELECT name FROM pets')}
+  erb :index
+end
+
+post '/pets' do
+  pet = params['pet_name']
+  db_connection do |conn|
+    conn.exec('INSERT INTO pets (name) VALUES ($1)', pet)
+  end
+  redirect '/pets'
+end
