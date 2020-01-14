c7cc57c85cb5374ae15be6ff8fe9ab5ece8ec958
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-14 15:55:09 -0500

    start activerecord-validations challenge

---
+++ activerecord-validations/server.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require_relative 'config/application'
+require "./app/models/song"
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+enable :sessions
+
+helpers do
+  def seconds_to_time(sec)
+    sec = sec.to_i
+    minutes = sec / 60
+    seconds = sec % 60
+    "#{minutes}:#{seconds.to_s.rjust(2, '0')}"
+  end
+end
+
+get "/" do
+  redirect to("/songs")
+end
+
+get "/songs" do
+  @songs = Song.all
+  erb :songs
+end
