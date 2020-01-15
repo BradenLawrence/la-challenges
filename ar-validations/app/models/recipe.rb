0460f9e549d80d8e76d3e595afd232a56aa227e2
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-15 13:36:15 -0500

    remove validation file, this is now handled directly in the recipe class

--- ar-validations/app/models/recipe.rb
+++ ar-validations/app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ActiveRecord::Base
   has_many :comments
   validates :title, uniqueness: true
   validates :title, presence: true
+  validates :title, format: {
+    with: /brussels sprouts/i,
+    message: "Title must contain 'brussels sprouts'."
+  }
+  validates :servings, presence: true
 end
--- ar-validations/app/models/validate_title.rb
+++
@@ -1,7 +0,0 @@
-# class ValidateTitle < ActiveRecord::EachValidator
-#   def validate_each(record)
-#     unless record.title.downcase.include? 'brussels sprouts'
-#       record.errors.add :title, "Title must include 'brussels sprouts'."
-#     end
-#   end
-# end
