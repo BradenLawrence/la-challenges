b7579f3328010df1a0e77e19b7a0c0114589f567
Braden Lawrence <lawrencebradenm@gmail.com>
2020-01-16 09:13:48 -0500

    add new crew form to starship show page and add form logic to server

--- star-fleet/server.rb
+++ star-fleet/server.rb
@@ -20,12 +20,11 @@ end
 post '/starships' do
   payload = Ship.new(params)
   if payload.valid?
-    payload.save
-    @new_ship = Ship.find_by(name: params["name"])
+    @new_ship = payload.save
     flash[:notice] = "Your new starship was created successfully!"
     redirect "/starships/#{@new_ship.id}"
   else
-    flash.now[:error] = "There was an error building your new starship. Please review your form and resubmit."
+    flash.now[:error] = @new_ship.errors.full_messages.to_sentence
     @name = params["name"]
     @ship_class = params["ship_class"]
     @location = params["location"]
@@ -48,4 +47,21 @@ end
 get '/crew-members' do
   @crew_members = CrewMember.all.order(:last_name)
   erb :'crew_members/index'
+end
+
+post '/crew-members' do
+  payload = CrewMember.new(params)
+  if payload.valid?
+    payload.save
+    flash[:notice] = "Successfully added new crew member!"
+    redirect '/crew-members'
+  else
+    binding.pry
+    flash.now[:error] = payload.errors.full_messages.to_sentence
+    @first_name = params["first_name"]
+    @last_name = params["last_name"]
+    @division = params["division"]
+    @ship = Ship.find(params["ship_id"])
+    erb :'starships/show'
+  end
 end
